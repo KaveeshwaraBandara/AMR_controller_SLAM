7941470997e086c91e1c4695b61bd766ccbea9aa
Kaveeshwara Bandara <imanchanabandara@gmail.com>
2025-05-15 11:43:45 +0530

    add sdk

--- Gazebo Simulation/src/main.cpp
+++ Gazebo Simulation/src/main.cpp
@@ -1,74 +1,42 @@
 #include <gz/transport/Node.hh>
-#include <gz/msgs/laserscan.pb.h>
+#include <gz/msgs/twist.pb.h>
+#include <thread>
+#include <chrono>
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <opencv2/opencv.hpp> // Add this
 
-struct Point2D {
-    float x;
-    float y;
-};
-
-std::vector<Point2D> latestPoints;
-
-void OnLaserScanMsg(const gz::msgs::LaserScan &msg)
+int main()
 {
-    latestPoints.clear();
-
-    float angle = msg.angle_min();
-    float angle_increment = msg.angle_step();
-
-    for (int i = 0; i < msg.ranges_size(); ++i)
-    {
-        float range = msg.ranges(i);
-        if (range > msg.range_min() && range < msg.range_max())
-        {
-            // Polar to Cartesian
-            float x = range * cos(angle);
-            float y = range * sin(angle);
-
-            latestPoints.push_back({x, y});
-        }
-        angle += angle_increment;
-    }
-
-    std::cout << "Received " << latestPoints.size() << " points." << std::endl;
-}
-
-int main(int argc, char **argv)
-{
+    // Create Gazebo Transport node
     gz::transport::Node node;
 
-    std::string topic = "/world/world_demo/model/tugbot/link/scan_front/sensor/scan_front/scan";
+    // Advertise the velocity topic
+    auto velPub = node.Advertise<gz::msgs::Twist>("/model/tugbot/cmd_vel");
 
-    if (!node.Subscribe(topic, &OnLaserScanMsg))
+    // Give Gazebo some time to connect
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::cout << "Publishing velocity commands to /model/tugbot/cmd_vel..." << std::endl;
+
+    // Create a Twist message to move forward
+    gz::msgs::Twist moveCmd;
+    moveCmd.mutable_linear()->set_x(0.5);  // Forward
+    moveCmd.mutable_angular()->set_z(0.0); // No rotation
+
+    // Send the move command for 3 seconds
+    for (int i = 0; i < 30; ++i)
     {
-        std::cerr << "Error subscribing to topic [" << topic << "]" << std::endl;
-        return -1;
+        velPub.Publish(moveCmd);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    std::cout << "Listening for LaserScan data on " << topic << " ..." << std::endl;
+    // Stop the robot
+    gz::msgs::Twist stopCmd;
+    stopCmd.mutable_linear()->set_x(0.0);
+    stopCmd.mutable_angular()->set_z(0.0);
+    velPub.Publish(stopCmd);
 
-while (true)
-{
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "Robot stopped." << std::endl;
 
-    // Draw current point cloud
-    cv::Mat map(500, 500, CV_8UC3, cv::Scalar(0,0,0));
-
-    for (const auto& point : latestPoints)
-    {
-        int x = static_cast<int>(point.x * 50) + 250; // Scale + center
-        int y = static_cast<int>(point.y * 50) + 250; // Scale + center
-
-        if (x >= 0 && x < 500 && y >= 0 && y < 500)
-            cv::circle(map, cv::Point(x, y), 1, cv::Scalar(0,255,0), -1);
-    }
-
-    cv::imshow("2D Lidar Map", map);
-    cv::waitKey(1);
-}
     return 0;
 }
 
